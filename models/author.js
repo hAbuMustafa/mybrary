e888d865ecd94fd52b67aa0940ced24a828960bf
Hosam Hamdy <hosam5553@gmail.com>
2022-12-02 16:28:20 +0200

    Starting with DB Connection

---
+++ models/author.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+
+cosnt authorSchema = new mongoose.Schema({
+  
+})
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').parse();
+  require('dotenv').config();
 }
 
 app.set('view engine', 'ejs');
@@ -10,7 +10,9 @@ app.set('views', `${__dirname}/views`);
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
-app.use('/', indexRouter);
+
+const indexRouter = require('./routes');
+const authorsRouter = require('./routes/authors');
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
@@ -20,5 +22,7 @@ const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
 
-const indexRouter = require('./routes');
+app.use('/', indexRouter);
+app.use('/authors', authorsRouter);
+
 app.listen(process.env.PORT || 3030);
